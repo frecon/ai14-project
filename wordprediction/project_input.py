bcf2094dcff4ebf2c286f2eb9d43199916aa27fb
Sanlil <sandra.liljeqvist@gmail.com>
2014-10-15 20:25:24 +0200

    Added a more generic function for getting ngrams in project_input.py and updated the test

--- wordprediction/project_input.py
+++ wordprediction/project_input.py
@@ -9,6 +9,14 @@ project_input.py: Reads an input file, creates a corpus and ngrams.
 
 """
 
+def get_ngrams(n, filename):
+    words = input_text(filename)
+    lowerWords = []
+    for word in words:
+        lowerWords.append(word.lower())
+    ngrams = nltk.ngrams(lowerWords, n)
+    return ngrams
+
 def get_trigrams(words):
     """From a list of words, trigrams are constructed and returned"""
     lowerWords = []
@@ -25,25 +33,23 @@ def get_bigrams(words):
     ngrams = bigrams(lowerWords)
     return ngrams
 
-
 def get_words(filename):
     """Creates a corpus from a text file"""
     words = nltk.corpus.genesis.words(filename)
     return words
 
 
-def input_text():
+def input_text(filename):
     """Takes a file name from the user and creates a corpus from the text the file contains"""
     path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    filename = sys.argv[-1]
     filepath = path +"/"+ filename
     words = get_words(filepath)
     return words
 
 
 def main():
-    words = input_text()
-    bigrams = get_bigrams(words)
+    filename = sys.argv[-1]
+    bigrams = get_ngrams(2, filename)
     for gram in bigrams:
         print (gram)
 
